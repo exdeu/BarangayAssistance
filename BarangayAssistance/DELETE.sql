698de0724fdc9921fee224a8d8b8a8374cd4f487
exdeu <excelsisdeu@gmail.com>
2026-04-29 14:59:47 +0800

    Sql queries 2

--- BarangayAssistance/DELETE.sql
+++
@@ -1,4 +0,0 @@
--- Delete a rejected application
-DELETE FROM assistance_applications
-WHERE application_id = 1
-AND   status = 'Rejected';
--- BarangayAssistance/UPDATE.sql
+++
@@ -1,6 +0,0 @@
--- Approve an assistance application
-UPDATE assistance_applications
-SET    status       = 'Approved',
-       date_updated = GETDATE()
-WHERE  application_id = 1
-AND    status = 'Pending';
