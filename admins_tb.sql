adab287a09f2b36b061894273094787c26d313c0
exdeu <excelsisdeu@gmail.com>
2026-05-04 22:21:36 +0800

    Minor changes

--- admins_tb.sql
+++
@@ -1,9 +0,0 @@
-﻿CREATE TABLE admins (
-    admin_id INT IDENTITY(1,1) PRIMARY KEY,
-    username VARCHAR(50) NOT NULL UNIQUE,
-    password_hash VARCHAR(255) NOT NULL,
-    full_name VARCHAR(150) NOT NULL,
-    role VARCHAR(50) NOT NULL DEFAULT 'Admin',
-    date_created DATETIME NOT NULL DEFAULT GETDATE(),
-    status VARCHAR(20) NOT NULL DEFAULT 'Active'
-)
